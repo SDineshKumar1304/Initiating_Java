9c842edc803a76adfe70ff24a25815ed0700feb7
S. Dinesh Kumar <125432987+SDineshKumar1304@users.noreply.github.com>
2024-06-05 11:05:05 +0530

    Update Interview2.java

--- java/Interview2.java
+++ java/Interview2.java
@@ -1,6 +1,6 @@
 class Interview2 {
     public static void main(String[] args) {
-        System.err.println("I am a Java Developer");// UI u need to  display the error eg :" Below 18 Cant Eligible to vote "
+        System.err.println("I am a Java Developer");// In UI or In the Terminal  u need to  display the error eg :" Below 18 Cant Eligible to vote " (Use Eclipse software to  see the output)
         System.out.println("I am a Java Developer");
     }
     

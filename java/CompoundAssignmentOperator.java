98ef3c7db682c8944ac90094853f43c24c71ca6f
S. Dinesh Kumar <125432987+SDineshKumar1304@users.noreply.github.com>
2024-06-09 20:41:20 +0530

    Update CompoundAssignmentOperator.java

--- java/CompoundAssignmentOperator.java
+++ java/CompoundAssignmentOperator.java
@@ -3,7 +3,7 @@ package Testjava.demo;
 public class CompoundAssignmentOperator {
 	public static void main(String[] args) {
 		int wallet = 500;
-		// if we do like this wallet = 300 ,200 will be loss 
+		// if we need to add 200 to the  wallet like this wallet = 300 ,200 will be loss 
 		// the correct way  is the below
 		wallet += 300;
 		 System.out.println("Updated amount : "+wallet);

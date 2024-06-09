d114371f65bbff20ab2f6ce3412b79799be3800e
S. Dinesh Kumar <125432987+SDineshKumar1304@users.noreply.github.com>
2024-06-09 20:55:35 +0530

    Update Modulus.java

--- java/Modulus.java
+++ java/Modulus.java
@@ -1,10 +1,10 @@
 package Testjava.demo;
 
-public class Modulus {
+public class ModulusDivisionOperator {
 
 	public static void main(String[] args) {
-		System.out.println(12345%10);
-		System.out.println(12345/10);
+		System.out.println(12345%10);// returns reminder 5
+		System.out.println(12345/10);// eliminates last 5 digit  returns 1234
 	}
 
 }

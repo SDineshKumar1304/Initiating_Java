4cfb62143ba6f873258f3e532a43f88d64f06b2d
S. Dinesh Kumar <125432987+SDineshKumar1304@users.noreply.github.com>
2024-06-09 20:53:37 +0530

    Update Interview4.java

--- java/Interview4.java
+++ java/Interview4.java
@@ -2,9 +2,10 @@ package Testjava.demo;
 
 public class Interview4 {
    public static void main(String[] args) {
-	int a= 10;
-	int b=a++;
+	int a= 10; // a got incremented and stored in  a 
+	int b=a++; // b stores initial a value
 	System.out.println(b);
 	System.out.print(b);
+	   // Returns 10 as output for b
 }
 }

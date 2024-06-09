c091af63ff383a402fc7c30af01bb66afecc71b0
S. Dinesh Kumar <125432987+SDineshKumar1304@users.noreply.github.com>
2024-06-09 20:49:07 +0530

    Update GlobLocal.java

--- java/GlobLocal.java
+++ java/GlobLocal.java
@@ -2,8 +2,9 @@ package Testjava.demo;
 
 public class GlobLocal {
 	static int a =2;
-	static int a =300;
+	static int a =300;// Global variable
 	public static void main(String[] args) {
+		// if int a =2 is declared in the main method called local  
 		System.out.println(a);
 	}
 

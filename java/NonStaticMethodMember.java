b61efaf49642e51e67112c42f1fd06fdfc007527
S. Dinesh Kumar <125432987+SDineshKumar1304@users.noreply.github.com>
2024-06-10 19:11:31 +0530

    Add files via upload

---
+++ java/NonStaticMethodMember.java
@@ -0,0 +1,11 @@
+package Dinesh.java;
+
+public class NonStaticMethodMember {
+	public void Dinesh() {
+		System.out.println("Hello");
+	}
+	public int Add(int a , int b){
+		int c = a+b;
+		return c;
+	}
+}
---
+++ java/UsingNonStaticMethod.java
@@ -0,0 +1,9 @@
+package Dinesh.java;
+
+public class UsingNonStaticMethod {
+   public static void main(String[] args) {
+	NonStaticMethodMember c = new NonStaticMethodMember();
+	System.out.println(c.Add(2, 0));
+	c.Dinesh();
+  }
+}

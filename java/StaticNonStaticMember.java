56f794ef14a706453a8a6596b3ea10d2e6267291
S. Dinesh Kumar <125432987+SDineshKumar1304@users.noreply.github.com>
2024-06-10 19:09:29 +0530

    Add files via upload

---
+++ java/StaticNonStaticMember.java
@@ -0,0 +1,14 @@
+package Testjava.demo;
+
+public class StaticNonStaticMember {
+	 
+	public void demo() {
+		System.out.println("Hello");
+	}
+	
+	public static void main(String[] args) {
+	//	demo();
+	//	StaticNonStaticMember.demo();//not possible CTE
+		
+	}
+}
